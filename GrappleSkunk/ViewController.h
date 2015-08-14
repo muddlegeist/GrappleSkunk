05883674589a6614b54978a9b44a28a150f295d9
Scott Puhl <muddlegeist@gmail.com>
2015-08-14 17:45:27 -0500

    Initial Commit

---
+++ GrappleSkunk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GrappleSkunk
+//
+//  Created by Muddlegeist on 8/14/15.
+//  Copyright (c) 2015 Muddlegeist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ GrappleSkunk/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GrappleSkunk
+//
+//  Created by Muddlegeist on 8/14/15.
+//  Copyright (c) 2015 Muddlegeist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
